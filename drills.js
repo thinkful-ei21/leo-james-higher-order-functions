8e67513cb10d99a834c4f381031eee4df561e857
Leo Veres <leoveres@gmail.com>
2018-05-08 14:16:46 -0400

    third commit

--- drills.js
+++ drills.js
@@ -16,3 +16,24 @@ function goodbye() {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+
+function filter(arr,fn) {
+    let newArray= [];
+    for (let i=0; i<arr.length;i++){
+        if(fn(arr[i])){
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+const testFunction = function(name) {
+    return name[0] === 'R';
+}
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Robert', 'Tony','Ray'];
+const filteredNames = filter(myNames, testFunction);
+
+console.log(filteredNames)
+
+  
